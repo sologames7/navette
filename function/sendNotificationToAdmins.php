d6e27ba2ab0e49e2370a74581dbaf94b5c544544
soulocean <93206786+sologames7@users.noreply.github.com>
2023-03-20 11:37:25 +0100

    appel a la bdd dans la fonction send to admins

--- function/sendNotificationToAdmins.php
+++ function/sendNotificationToAdmins.php
@@ -1,5 +1,25 @@
 <?php
-    echo "<script>
+    include_once('function/dbConnect.php');
+    function sendNotificationToAdmins($pushMessage){
+        try{
+            $tokenTable = [];
+            $bdd1 = dbConnect();
+            $sql = "SELECT * FROM `user` WHERE us_permission='admin'";
+            $reponse = $bdd1->query($sql);
+            
+            if($reponse->rowCount() > 0){
+                while($donnees = $reponse->fetch()){
+                    array_push($tokenTable, $donnees["us_token"]);
+                }
+            }else{
+                return false;
+            }
+            $bdd1 = null;
+            } catch (PDOException $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        echo "<script>
             const sendNotificationRequest = async (tokenTable, pushMessage) => {  const response = await fetch('https://425c94f.online-server.cloud:3443/send-notification', {
                 method: 'post',
                 headers: {
@@ -14,8 +34,6 @@
                 console.log(data)
                 }
          </script>";
-
-    function sendNotificationToAdmins($tokenTable, $pushMessage){
         $jscode = "sendNotificationRequest($tokenTable ,$pushMessage)";
         echo "<script>eval($jscode);</script>";
     }
